c420d2af3ef8344f71b25ebeb61e1e63bbaa8055
Prajjwal Sahu <prajjwal2051@gmail.com>
2025-03-30 18:57:50 +0530

    solved max_sum_subarray_efficient

--- Arrays/max_sum_subarray.cpp
+++ Arrays/max_sum_subarray.cpp
@@ -14,6 +14,18 @@ int max_sum_subarray(int arr[], int n){
     return res;
 }
 
+// this is the efficient method
+
+int maxSumm(int arr[], int n){
+    int res=arr[0];
+    int maxending=arr[0];
+    for(int i=1;i<n;i++){
+        maxending=max(maxending+arr[i],arr[i]);
+        res=max(res,maxending);
+    }
+    return res;
+}
+
 
 
 int main(){
@@ -23,6 +35,9 @@ int main(){
 
     int result=max_sum_subarray(arr,n);
     cout<<result<<endl;
+
+    int result2=maxSumm(arr,n);
+    cout<<result2<<endl;
 
 
 
