2fae33a8b72c4ece60bfbd90abcfd7c8a57544e1
Prajjwal Sahu <prajjwal2051@gmail.com>
2025-04-06 10:04:15 +0530

    minimum consecutive flips

--- Arrays/minimum_consecutive_flips.cpp
+++ Arrays/minimum_consecutive_flips.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
 
+bool printgroups(int arr[], int n){
+    for(int i=1;i<n;i++){
+        if(arr[i-1]!=arr[i]){
+            if(arr[i]!=arr[0]){
+                cout<<"From "<<i<<" to ";
+            }
+            else{
+                cout<<i-1<<endl;
+            }
+        }
+    }
+    if(arr[n-1]!=arr[0]){
+        cout<<n-1<<endl;
+    }
+}
+
+
 int main(){
 
-    
+    // given array we need to flips the minimum number of bits to make it all 0 or all 1
+    int arr[]={1,0,0,1,0,0,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    printgroups(arr,n);
 
     return 0;
 
