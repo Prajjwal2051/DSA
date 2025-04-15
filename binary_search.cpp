b5fe37e488f747d4abd6deac605d4506b192d5ff
Prajjwal Sahu <prajjwal2051@gmail.com>
2025-04-15 18:43:20 +0530

    Binary Search

---
+++ binary_search.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int linear_search(int arr[], int n, int key){
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+// now using the binary search
+
+int bSearch(int arr[], int n, int key){
+    int low=0, high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>key){
+            high=mid-1;
+        }
+        if(arr[mid]<key){
+            low=mid+1;
+        }
+        if(arr[mid]==key){
+            return mid;
+        }
+    }
+    return -1;
+}
+
+
+int main(){
+
+    int arr[]={10,20,30,40,50,60,70};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int key=50;
+    int res1=linear_search(arr,n,key);
+    cout<<res1<<endl;
+    int res2=bSearch(arr,n,key);
+    cout<<res2<<endl;
+    return 0;
+
+}
