321cd6128cbd6eadf23d2adf5d69db2b6f8c33de
Prajjwal Sahu <prajjwal2051@gmail.com>
2025-04-17 18:29:10 +0530

    occurances of 1

---
+++ Search/counts_1_in_binary.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int countOnes(int arr[], int n){
+    int low=0, high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if (arr[mid]==0){
+            low=mid+1;
+        }
+        else{
+            if(mid==0 || arr[mid-1]!=arr[mid]){
+                return (n-mid);
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+}
+
+int main(){
+
+    int arr[]={0,0,0,0,1,1,1,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<"number of 1s is "<<countOnes(arr,n)<<endl;
+
+    return 0;
+
+}
