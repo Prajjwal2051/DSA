530d06188308566d502a2d30cf4d7ee110c1ea30
Prajjwal Sahu <prajjwal2051@gmail.com>
2025-04-18 13:46:24 +0530

    sqaure root

---
+++ Search/sqaure_root.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int sqaureRoot(int n){
+    int low=1,high=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        int msq=mid*mid;
+        if(msq==n){
+            return mid;
+        }
+        else if(msq>n){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return -1;  // -1 is comming becuare it is not the perfect square
+}
+
+
+int main(){
+
+    cout<<sqaureRoot(17)<<endl;
+
+    return 0;
+
+}
